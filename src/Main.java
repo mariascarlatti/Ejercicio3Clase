4b286df839788f110cf7d18a42d5dabbc5bf52ed
Maria <9201373@alumnos.ufv.es>
2025-06-07 19:40:12 +0200

    Created calculator's menu.

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido a la calcladora de la clase main!");
     }
+
+    public void menu() {
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
+        System.out.println("5. Salir");
+    }
 }
