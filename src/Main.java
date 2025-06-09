2eef88bbc44b0f386188340f709f8c24eebe65fc
Maria <9201373@alumnos.ufv.es>
2025-06-09 17:47:04 +0200

    Added substract to menu and calculator

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,15 @@ public class Main {
                     System.out.println("Resultado: " + operations.sum(num1, num2));
                     menu();
                     break;
+                case 2:
+                    System.out.println("Seleccionó Restar");
+                    System.out.print("Ingrese el primer número: ");
+                    int num3 = Integer.parseInt(scanner.nextLine());
+                    System.out.print("Ingrese el segundo número: ");
+                    int num4 = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Resultado: " + operations.subtract(num3, num4));
+                    menu();
+                    break;
                 default:
                     System.out.println("Opción no válida. Por favor, intente de nuevo.");
                     menu();
